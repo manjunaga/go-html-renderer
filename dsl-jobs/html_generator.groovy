192054becbf1adc9fc9a73dde09e0c2aca40da1f
Manjunatha Nagaraja <nmanjunatha@gmail.com>
2025-01-08 16:48:24 +0000

    Adds dsl-jobs

---
+++ dsl-jobs/html_generator.groovy
@@ -0,0 +1,25 @@
+pipelineJob('html-generator') {
+    parameters {
+        stringParam('DEPLOYED_BY',
+            defaultValue = '',
+            description = 'Identifies who generated the latest HTML.'
+        )
+    }
+    
+    parameters {...}
+ 
+    definition {
+        cpsScm {
+            scm {
+                git {
+                    remote { 
+                        url('https://github.com/manjunaga/go-html-renderer.git') // Your Github repo
+                        credentials("go-html-renderer") // Credentials ID from the previous step
+                    }
+                    branches('dynamic-html')
+                    scriptPath('Jenkinsfile')
+                }
+            }
+        }
+    }
+}
